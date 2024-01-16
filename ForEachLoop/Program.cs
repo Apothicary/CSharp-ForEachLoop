787b77f4104a5e15792deeb1e799a0387cba3373
Nathan Strang <n.strang95@gmail.com>
2024-01-16 21:21:34 +0000

    Adding program code

---
+++ ForEachLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ForEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare an array to store car brands, with space for three elements
+            string[] cars = new string[3];
+
+            // Assign values to array elements
+            cars[0] = "Tesla";
+            cars[1] = "BMW";
+            cars[2] = "Toyota";
+
+            // Iterate through the array using foreach and print each car brand
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            // Wait for user input before closing the console window
+            Console.ReadKey();
+        }
+    }
+}
